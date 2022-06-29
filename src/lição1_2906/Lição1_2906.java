ca8abdb8404b0df5d3c772f79055e89d95e9f790
João Rafael Rodrigues Lopes <182200150@senacinformatica.br>
2022-06-29 14:58:34 -0300

    Primeiro envio

---
+++ src/lição1_2906/Lição1_2906.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lição1_2906;
+
+/**
+ *
+ * @author 182200150
+ */
+public class Lição1_2906 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        System.out.println("Joao");
+            
+    }
+    
+}
